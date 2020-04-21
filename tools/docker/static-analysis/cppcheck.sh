176720ae899ba75c7966d47884d9bb1bc0ff7b8c
Raphaël Mélotte <raphael.melotte@mind.be>
2020-04-21 16:35:41 +0200

    tools: cppcheck: always sort the issues alphabetically
    This is a preparatory commit. Having the issues sorted makes it easier
    to later compare them.
    
    Signed-off-by: Raphaël Mélotte <raphael.melotte@mind.be>

--- tools/docker/static-analysis/cppcheck.sh
+++ tools/docker/static-analysis/cppcheck.sh
@@ -33,7 +33,7 @@ run_cppcheck() {
              -j"$(nproc)" \
              -q \
              $INCLUDES \
-             "$@" 2>&1 | tee "$OUTPUT_FILE"
+             "$@" 2>&1 | sort | tee "$OUTPUT_FILE"
 }
 
 colorize_severity() {
